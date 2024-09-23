7a9bdf690304f380bea10a928fc663ab4188b896
Yann Forget <yannforget@mailbox.org>
2024-09-23 17:15:54 +0200

    feat: add era5 download modules

---
+++ openhexa/toolbox/era5/cds.py
@@ -0,0 +1,142 @@
+from __future__ import annotations
+
+import importlib.resources
+import json
+import logging
+import shutil
+import tempfile
+from calendar import monthrange
+from datetime import datetime
+from pathlib import Path
+
+import cads_api_client
+import cdsapi
+
+with importlib.resources.open_text("openhexa.toolbox.era5", "variables.json") as f:
+    VARIABLES = json.load(f)
+
+DATASET = "reanalysis-era5-land"
+
+logging.basicConfig(level=logging.DEBUG, format="%(name)s %(asctime)s %(levelname)s %(message)s")
+log = logging.getLogger(__name__)
+
+URL = "https://cds-beta.climate.copernicus.eu/api"
+
+
+class ParameterError(ValueError):
+    pass
+
+
+class Client:
+    def __init__(self, key: str):
+        self.client = cdsapi.Client(url=URL, key=key, wait_until_complete=True, quiet=True, progress=False)
+        self.cads_api_client = cads_api_client.ApiClient(key=key, url=URL)
+
+    def latest(self) -> datetime:
+        """Get date of latest available product."""
+        collection = self.cads_api_client.collection(DATASET)
+        _, end = collection.json["extent"]["temporal"]["interval"][0]
+        end = datetime.strptime(end, "%Y-%m-%dT00:00:00Z")
+        return end
+
+    @staticmethod
+    def build_request(
+        variable: str,
+        year: int,
+        month: int,
+        days: list[int] = None,
+        time: list[str] = None,
+        data_format: str = "grib",
+        area: list[float] = None,
+    ) -> dict:
+        """Build request payload.
+
+        Parameters
+        ----------
+        variable : str
+            Climate data store variable name (ex: "2m_temperature").
+        year : int
+            Year of interest.
+        month : int
+            Month of interest.
+        days : list[int]
+            Days of interest. Defauls to None (all days).
+        time : list[str]
+            Hours of interest (ex: ["01:00", "06:00", "18:00"]). Defaults to None (all hours).
+        data_format : str
+            Output data format ("grib" or "netcdf"). Defaults to "grib".
+        area : list[float]
+            Area of interest (north, west, south, east). Defaults to None (world).
+
+        Returns
+        -------
+        dict
+            Request payload.
+
+        Raises
+        ------
+        ParameterError
+            Request parameters are not valid.
+        """
+        if variable not in VARIABLES:
+            raise ParameterError("Variable %s not supported", variable)
+
+        if data_format not in ["grib", "netcdf"]:
+            raise ParameterError("Data format %s not supported", data_format)
+
+        if area:
+            n, w, s, e = area
+            if ((abs(n) > 90) or (abs(s) > 90)) or ((abs(w) > 180) or (abs(e) > 180)):
+                raise ParameterError("Invalid area of interest")
+            if (n < s) or (e < w):
+                raise ParameterError("Invalid area of interest")
+
+        if not days:
+            dmax = monthrange(year, month)[1]
+            days = [day for day in range(1, dmax + 1)]
+
+        if not time:
+            time = [f"{hour:02}:00" for hour in range(0, 24)]
+
+        year = str(year)
+        month = f"{month:02}"
+        days = [f"{day:02}" for day in days]
+
+        payload = {
+            "variable": [variable],
+            "year": year,
+            "month": month,
+            "day": days,
+            "time": time,
+            "data_format": data_format,
+        }
+
+        if area:
+            payload["area"] = area
+
+        return payload
+
+    def download(self, request: dict, dst_file: str | Path, overwrite: bool = False):
+        """Download Era5 product.
+
+        Parameters
+        ----------
+        request : dict
+            Request payload as returned by the build_request() method.
+        dst_file : Path
+            Output file path.
+        overwrite : bool, optional
+            Overwrite existing file (default=False).
+        """
+        dst_file = Path(dst_file)
+        dst_file.parent.mkdir(parents=True, exist_ok=True)
+
+        if dst_file.exists() and not overwrite:
+            log.debug("File %s already exists, skipping download", str(dst_file.absolute()))
+            return
+
+        with tempfile.NamedTemporaryFile() as tmp:
+            self.client.retrieve(name=DATASET, request=request, target=tmp.name)
+            shutil.move(tmp.name, dst_file)
+
+        log.debug("Downloaded Era5 product to %s", str(dst_file.absolute()))
---
+++ openhexa/toolbox/era5/google.py
@@ -0,0 +1,121 @@
+"""Download raw historical Era5 products from Google Cloud:
+https://console.cloud.google.com/storage/browser/gcp-public-data-arco-era5
+
+Products are provided as raw NetCDF files and are usually available with a ~3 month lag.
+"""
+
+from __future__ import annotations
+
+import importlib.resources
+import json
+import logging
+import shutil
+import tempfile
+from datetime import datetime
+from functools import cached_property
+from pathlib import Path
+
+import requests
+from google.cloud import storage
+
+with importlib.resources.open_text("openhexa.toolbox.era5", "variables.json") as f:
+    VARIABLES = json.load(f)
+
+logging.basicConfig(level=logging.DEBUG, format="%(name)s %(asctime)s %(levelname)s %(message)s")
+log = logging.getLogger(__name__)
+
+
+class NotFoundError(Exception):
+    pass
+
+
+class ParameterError(ValueError):
+    pass
+
+
+class Client:
+    def __init__(self):
+        self.client = storage.Client.create_anonymous_client()
+        self.bucket = self.client.bucket("gcp-public-data-arco-era5")
+
+    @staticmethod
+    def prefix(variable: str, date: datetime) -> str:
+        """Build key prefix for a given product."""
+        return f"raw/date-variable-single_level/{date.year}/{date.month:02}/{date.day:02}/{variable}/surface.nc"
+
+    def _subdirs(self, prefix: str) -> list[str]:
+        """List subdirs."""
+        blobs = self.client.list_blobs(self.bucket, prefix=prefix, delimiter="/")
+        prefixes = []
+        for page in blobs.pages:
+            prefixes += page.prefixes
+        return prefixes
+
+    @cached_property
+    def latest(self) -> datetime:
+        """Get date of latest available product."""
+        root = "raw/date-variable-single_level/"
+        subdirs = self._subdirs(root)  # years
+        subdirs = self._subdirs(max(subdirs))  # months
+        subdirs = self._subdirs(max(subdirs))  # days
+        subdir = max(subdirs).split("/")
+        year = int(subdir[-4])
+        month = int(subdir[-3])
+        day = int(subdir[-2])
+        return datetime(year, month, day)
+
+    def find(self, variable: str, date: datetime) -> str | None:
+        """Find public URL of product. Return None if not found."""
+        prefix = self.prefix(variable, date)
+        blobs = self.client.list_blobs(self.bucket, prefix=prefix, max_results=1)
+        blobs = list(blobs)
+        if blobs:
+            return blobs[0].public_url
+        else:
+            return None
+
+    def download(self, variable: str, date: datetime, dst_file: str | Path, overwrite=False):
+        """Download an Era5 NetCDF product for a given day.
+
+        Parameters
+        ----------
+        variable : str
+            Climate data store variable name (ex: "2m_temperature").
+        date : datetime
+            Product date (year, month, day).
+        dst_file : str | Path
+            Output file.
+        overwrite : bool, optional
+            Overwrite existing file (default=False).
+
+        Raises
+        ------
+        ParameterError
+            Product request parameters are invalid.
+        NotFoundError
+            Product not found in bucket.
+        """
+        dst_file = Path(dst_file)
+        dst_file.parent.mkdir(parents=True, exist_ok=True)
+
+        if dst_file.exists() and not overwrite:
+            log.debug("Skipping download of %s because file already exists", str(dst_file.absolute()))
+            return
+
+        if variable not in VARIABLES:
+            raise ParameterError("%s is not a valid climate data store variable name", variable)
+
+        url = self.find(variable, date)
+        if not url:
+            raise NotFoundError("%s product not found for date %s", variable, date.strftime("%Y-%m-%d"))
+
+        with tempfile.NamedTemporaryFile() as tmp:
+            with open(tmp.name, "wb") as f:
+                with requests.get(url, stream=True) as r:
+                    for chunk in r.iter_content(chunk_size=1024**2):
+                        if chunk:
+                            f.write(chunk)
+
+            shutil.move(tmp.name, dst_file)
+
+        log.debug("Downloaded %s", str(dst_file.absolute()))
