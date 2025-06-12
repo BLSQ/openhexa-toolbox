a42b747becb24cc194c026af060b990eb7f3dbb6
nazarfil <nfilipchuk@bluesquarehub.com>
2025-06-12 09:37:44 +0200

    tests emit event

--- tests/lineage/test_lineage.py
+++ tests/lineage/test_lineage.py
@@ -1,6 +1,10 @@
-from http.client import responses
+import json
+from datetime import timezone, datetime
 
 import pytest
+import responses
+
+from tests.lineage.fixtures.lineage import emit_event_example
 
 
 class TestLineage:
@@ -22,18 +26,20 @@ class TestLineage:
         from openhexa.toolbox.lineage.client import OpenHexaOpenLineageClient
         from openlineage.client.event_v2 import RunState
 
-        lineage.init_client(url="http://localhost:8080", workspace_slug="default", pipeline_slug="test_pipeline")
+        lineage.init_client(url="http://localhost:3000", workspace_slug="default", pipeline_slug="test_pipeline", pipeline_run_id = "abe36e6a-8af7-4718-a753-c6d2054d1ecf")
 
         client: OpenHexaOpenLineageClient = lineage._client
-        mock_responses.add(responses.POST, "http://localhost:8080/api/v1/lineage/events", json={}, status=200)
+        mock_responses.add(responses.POST, "http://localhost:3000/api/v1/lineage", json={}, status=200)
 
         client.emit_run_event(
             event_type=RunState.START,
             task_name="test_task",
             inputs=[],
             outputs=[],
-            start_time=None,
-            end_time=None,
-            sql=None,
+            start_time=datetime(2025, 6, 12, 7, 24, 48, 727726, tzinfo=timezone.utc),
+            end_time=datetime(2025, 6, 12, 7, 27, 48, 727726, tzinfo=timezone.utc)
         )
-        assert mock_responses.calls[0].request.url == "http://localhost:8080/api/v1/lineage/events"
+
+        assert mock_responses.calls[0].request.url == "http://localhost:3000/api/v1/lineage"
+        assert mock_responses.calls[0].request.method == "POST"
+        assert json.loads(mock_responses.calls[0].request.body) == json.loads(mock_responses.calls[0].request.body)
